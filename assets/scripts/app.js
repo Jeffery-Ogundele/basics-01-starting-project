d0709e82a3d555c1066ee06410d3b598fb55f8fb
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-17 18:07:08 +0100

    Using string concatenation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,6 @@
 
  currentResult = (currentResult + 10) * 3 / 2 - 1 ;
 
- let calculationDecription = '(0 + 10) * 3 / 2 - 1'  
+ let calculationDecription = '(' + currentResult + ' + 10) * 3 / 2 - 1'  
 
  outputResult(currentResult, calculationDecription);
