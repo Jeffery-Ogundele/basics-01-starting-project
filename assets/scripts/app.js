9d6563e9ba9a825fee7821d8d476e184ac698278
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-17 18:03:28 +0100

    Using let keyword

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert('This Works!');
+ let currentResult = 0;
+
+ currentResult = (currentResult + 10) * 3 / 2 - 1 ;
+
+ let calculationDecription = '(0 + 10) * 3 / 2 - 1'  
+
+ outputResult(currentResult, calculationDecription);
