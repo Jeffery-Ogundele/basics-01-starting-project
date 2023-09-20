8324fad8f584dfeaabb5b0cfa0b0487555c4eb00
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-20 22:15:18 +0100

    Adding a function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1 ;
+function add (num1, num2) {
+    const result = num1 + num2;
+    alert(`the result is ${result}`)
+}
+add (1,3)
+add (0,4)
 
- let calculationDecription =  `( ${defaultResult} + 10) * 3 / 2 - 1`;
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
- outputResult(currentResult, calculationDecription);
+let calculationDecription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDecription);
