5629a429005fc1d6e5995b3e73d6a5932cb06f75
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-21 19:41:48 +0100

    Adding event listener

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add (num1, num2) {
-    const result = num1 + num2;
-    alert(`the result is ${result}`)
+function add ( ) {
+   currentResult = currentResult + userInput.value;
+   outputResult(currentResult,  '');
 }
-add (1,3)
-add (0,4)
+ 
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+addBtn.addEventListener('click', add);
 
-let calculationDecription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDecription);
+ 
