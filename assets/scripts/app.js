8b621139d3f80b30414983f4987b8c55209e7798
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-24 18:13:47 +0100

    connecting all the buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,6 +12,29 @@ function add ( ) {
 }
  
 
+function subtract() {
+   const enteredNumber  = getUserInput();
+   const calcDescription = `${currentResult} - ${enteredNumber}`
+   currentResult = currentResult - enteredNumber;
+   outputResult(currentResult,  calcDescription);
+}
+
+
+function multiply() {
+   const enteredNumber  = getUserInput();
+   const calcDescription = `${currentResult} * ${enteredNumber}`
+   currentResult = currentResult * enteredNumber;
+   outputResult(currentResult,  calcDescription);
+}
+
+function divide () {const enteredNumber  = getUserInput();
+   const calcDescription = `${currentResult} / ${enteredNumber}`
+   currentResult = currentResult / enteredNumber;
+   outputResult(currentResult,  calcDescription);}
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click',divide);
+
  
- 
+//all the buttons work :) 
