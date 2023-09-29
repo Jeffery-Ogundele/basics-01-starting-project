65af0df068dfeeb6c12c2a44e2f8d9408a1d8f75
Ogundele Jeffery <jefferyogundele7@gmail.com>
2023-09-29 10:32:27 +0100

    Importing scripts correctly using defer and async

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,19 +9,25 @@ function createAndWriteOutput(operator, resultBefotrCalc, calcNumber) {
   const calcDescription = `${resultBefotrCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
+
+function writeToLog (operationIdentifier,prevResult,operationNumber,newResult) {
+   const logEntry = {
+      operation: operationIdentifier,
+      prevResult: prevResult,
+      number: operationNumber,
+      result: newResult,
+    };
+    logEntries.push(logEntry);
+  console.log(logEntries );
+
+}
 function add() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
-  const logEntry = {
-    operation: "ADD",
-    prevResult: initialResult,
-    number: enteredNumber,
-    result: currentResult,
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries );
+  writeToLog('ADD',initialResult,enteredNumber,currentResult)
+   
 }
 
 function subtract() {
@@ -29,6 +35,7 @@ function subtract() {
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog('SUBTRACT',initialResult,enteredNumber,currentResult)
 }
 
 function multiply() {
@@ -36,6 +43,7 @@ function multiply() {
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog('MULTIPLY',initialResult,enteredNumber,currentResult)
 }
 
 function divide() {
@@ -43,6 +51,7 @@ function divide() {
   const initialResult = currentResult;
   currentResult /= enteredNumber;
   createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog('DIVIDE',initialResult,enteredNumber,currentResult)
 }
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
